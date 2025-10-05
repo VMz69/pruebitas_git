c2381f6ad4595da9cb35305a61a300a0859f0a11
Vmz69 <viemvema@gmail.com>
2025-10-05 01:19:14 -0600

    arreglando rutas 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 const basePath = location.hostname === "vmz69.github.io"
     ? "/pruebitas_git/"
-    : "./";
+    : "../";
 
 async function cargarPartial(nombre) {
     try {
-        const ruta = `${basePath}../partials/${nombre}.html`;
+        const ruta = `${basePath}partials/${nombre}.html`;
         const resp = await fetch(ruta, { cache: "no-cache" }); // Realiza la petición HTTP para obtener el contenido del partial
         if (!resp.ok) throw new Error(`Error al cargar ${ruta}: ${resp.status}`); // Si la respuesta no es correcta (404, 500, etc.), lanza un error
 
@@ -20,7 +20,7 @@ async function cargarPartial(nombre) {
     }
 }
 
-// --- llamadas al final ---
+
 cargarPartial('header');
 cargarPartial('footer');
 
